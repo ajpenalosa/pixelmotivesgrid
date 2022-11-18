e1170c1a9367fda635617e9d3d71dde2b96b6bc2
AJ Penalosa <ajpenalosa@gmail.com>
2022-11-17 21:55:00 -0800

    Set up grid

--- public/index.php
+++ public/index.php
@@ -8,6 +8,218 @@
     <link href="assets/css/style.min.css" rel="stylesheet">
 </head>
 <body>
-    
+
+    <div class="pm-site-container">
+
+        <?php /*----- One Column -----*/ ?>
+
+        <div class="pm-grid pm-grid pm-grid--contained">
+
+            <div class="pm-grid__container">
+
+                <div class="pm-grid__column pm-grid__column--12-columns">
+                    
+                    <h2>Has One Column</h2>
+
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--12-columns">
+                    
+                    <h3>Column 1</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+            </div>
+            
+        </div>
+
+        <?php /*----- Two Columns -----*/ ?>
+
+        <div class="pm-grid pm-grid pm-grid--contained">
+
+            <div class="pm-grid__container">
+
+                <div class="pm-grid__column pm-grid__column--12-columns">
+                    
+                    <h2>Has Two Columns</h2>
+
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--6-columns">
+                    
+                    <h3>Column 1</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--6-columns">
+                    
+                    <h3>Column 2</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+            </div>
+            
+        </div>
+
+        <?php /*----- Three Columns -----*/ ?>
+
+        <div class="pm-grid pm-grid pm-grid--contained">
+
+            <div class="pm-grid__container">
+
+                <div class="pm-grid__column pm-grid__column--12-columns">
+                    
+                    <h2>Has Three Columns</h2>
+
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--4-columns">
+                    
+                    <h3>Column 1</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--4-columns">
+                    
+                    <h3>Column 2</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--4-columns">
+                    
+                    <h3>Column 3</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+            </div>
+            
+        </div>
+
+        <?php /*----- Four Columns -----*/ ?>
+
+        <div class="pm-grid pm-grid pm-grid--contained">
+
+            <div class="pm-grid__container">
+
+                <div class="pm-grid__column pm-grid__column--12-columns">
+                    
+                    <h2>Has Four Columns</h2>
+
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--3-columns">
+                    
+                    <h3>Column 1</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--3-columns">
+                    
+                    <h3>Column 2</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--3-columns">
+                    
+                    <h3>Column 3</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--3-columns">
+                    
+                    <h3>Column 4</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+            </div>
+            
+        </div>
+
+        <?php /*----- Six Columns -----*/ ?>
+
+        <div class="pm-grid pm-grid pm-grid--contained">
+
+            <div class="pm-grid__container">
+
+                <div class="pm-grid__column pm-grid__column--12-columns">
+                    
+                    <h2>Has Six Columns</h2>
+
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--2-columns">
+                    
+                    <h3>1</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--2-columns">
+                    
+                    <h3>2</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--2-columns">
+                    
+                    <h3>3</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--2-columns">
+                    
+                    <h3>4</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--2-columns">
+                    
+                    <h3>5</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+                <div class="pm-grid__column pm-grid__column--2-columns">
+                    
+                    <h3>6</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu, ac eleifend nulla. Suspendisse eleifend ultrices viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sodales pharetra bibendum. Proin orci purus, commodo in vestibulum vel, rhoncus vitae arcu. Praesent tincidunt fringilla sollicitudin. Cras egestas nisi urna, consectetur porttitor magna molestie non. Etiam accumsan, ex ac egestas finibus, eros justo sagittis justo, sed tincidunt ipsum erat quis ligula.</p>
+                    
+                </div>
+
+            </div>
+            
+        </div>
+
+    </div>
+        
 </body>
 </html>
